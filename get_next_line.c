993dd568578740e0a7c99229dc167797b6f5efd6
Anas Rchid <archid-@student.1337.ma>
2019-04-16 04:53:32 +0100

    bak: create proper cache functions
    TODO:
    =====
    
    	+ create cache_concat()
    	+ use cache_free() and cache_alloc() to handle the cache

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,130 @@
 /*   By: archid- <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 18:01:11 by archid-           #+#    #+#             */
-/*   Updated: 2019/04/15 04:05:46 by archid-          ###   ########.fr       */
+/*   Updated: 2019/04/16 04:53:24 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** TODO: remove unused headers
+*/
+/*
+** TODO: remove comments
+*/
+/*
+** TODO: check the norm
+*/
+/*
+**TODO: improve the code
+*/
+/*
+** FIXEME: reorder e_fuzzbuzz
+*/
 
 #include <fcntl.h>
 #include <stdio.h>
 
 #include "get_next_line.h"
 
+/*
+** TODO: ft_realloc()
+*/
+
+int	   cache_alloc(char **s, t_bool isfull)
+{
+	char *tmp[2];
+
+	if (s == NULL)
+		return (failure);
+	else if (*s == NULL)
+		*s = ALLOC(char *, CACHE_SIZE);
+	else if (*s && isfull)
+	{
+		if (!(tmp[0] = ALLOC(char *, S_LEN(tmp[0]) + CACHE_SIZE)))
+			return (failure);
+		ft_memcpy(tmp[0], *s, S_SIZE(tmp[0]));
+		tmp[1] = *s;
+		free(*s);
+		*s = tmp[1];
+	}
+	return (success);
+}
+
+void	cache_free(char **s, t_bool has_nl)
+{
+	char *tmp;
+
+	if (!s || !*s)
+	{
+		if (has_nl)
+		{
+			tmp = *s;
+			*s = ft_strdup(ft_strchr(*s, NL) + 1);
+			free(tmp);
+		}
+		else
+		{
+			/* FIXME: something is missing here */
+		}
+	}
+}
+
+/*
+** this would concatinate s1 with s2, and reallocate memory if
+** nessecary i.e. when (S_LEN(foo) >= size - 1).
+**
+** s1 is allocated using cache_alloc() and free'd using cache_free()
+*/
+
+char	*cache_concat(char *s1, const char *s2, size_t size)
+{
+
+	return (s1);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static char *cache[0xFF] = {NULL};
-	char		*tmp[count];
+	char		*tmp[icount];
 	size_t		nbytes;
 
-	if (!(tmp[buffer] = ALLOC(char *, BUFF_SIZE + 1)))
+	/* TODO: add a cache_alloc() somwhere in here */
+
+	/* 0.1 check if memory is allocated */
+	if (!(tmp[ibuffer] = ALLOC(char *, BUFF_SIZE + 1)))
 		return (failure);
-	if (fd < 0 || !line || read(fd, tmp[buffer], 0) < 0)
+	/* 0.2 check if fd, and line and we can read the file */
+	if (fd < 0 || !line || read(fd, tmp[ibuffer], 0) < 0)
 		return (failure);
-	if (cache[fd] && (tmp[bar] = ft_strchr(cache[fd], 0x0A)))
+	/* 1.0 check if cache has already a NL */
+	if (cache[fd] && (tmp[ibar] = ft_strchr(cache[fd], NL)))
 	{
-		*line = ft_strrdup(cache[fd], tmp[bar] - 1);
-		cache[fd] = tmp[bar] + 1;
+		/* if so duplicated from from 0 to index of NL */
+		*line = ft_strrdup(cache[fd], tmp[ibar] - 1);
+		// cache[fd] = tmp[ibar] + 1;
+		/* free copied part of the cache using cache_free() */
+		cache_free(&cache[fd], tmp[ibar] != NULL);
 		return (success);
 	}
-	while ((nbytes = read(fd, tmp[buffer], BUFF_SIZE)))
+	/* 2.0 if the cache does not have a NL in it, we read a BUFF_SIZE
+	 * chars into our tmporary buffer */
+	while ((nbytes = read(fd, tmp[ibuffer], BUFF_SIZE)))
 	{
-		if (!(cache[fd] = ft_strjoin(!cache[fd] ? "" : cache[fd], tmp[buffer])))
+		/* 2.1 join the read chars with the previous cache content */
+		if (!(cache[fd] = ft_strjoin(cache[fd] ? cache[fd] : "", tmp[ibuffer])))
 			return (failure);
-		if ((tmp[bar] = ft_strchr(tmp[buffer], 0x0A)))
+		/* 2.2 if we have read a NL we stop reading from fd */
+		if ((tmp[ibar] = ft_strchr(tmp[ibuffer], NL)))
 			break ;
 	}
-	if (nbytes < BUFF_SIZE && !ft_strlen(tmp[buffer]))
+	/* 2.3 if we have read less than BUFF_SIZE and the temporary buffer is
+	 * empty, then it must be an EOF */
+	if (nbytes < BUFF_SIZE && !ft_strlen(tmp[ibuffer]))
 		return (eof);
-	*line = ft_strrdup(cache[fd], ft_strchr(cache[fd], 0x0A) - 1);
-	(void)
-		(!*(tmp[bar] + 1) ? ft_strclr(cache[fd]) : (cache[fd] = tmp[bar] + 1));
+	/* 3.0 copy from cache until NL */
+	*line = ft_strrdup(cache[fd], (tmp[ifoo] = ft_strchr(cache[fd], NL)) - 1);
+	/* 3.1 free the copied version */
+	cache_free(&cache[fd], tmp[ifoo]!= NULL);
 	return (success);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,48 @@
 /*   By: archid- <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 18:01:16 by archid-           #+#    #+#             */
-/*   Updated: 2019/04/15 03:57:53 by archid-          ###   ########.fr       */
+/*   Updated: 2019/04/16 03:37:12 by archid-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE				10
-
 # include <unistd.h>
 # include "libft/libft.h"
 
+# define BUFF_SIZE				10
+# define CACHE_SIZE				(2 * BUFF_SIZE + 1)
+# define S_LEN(s)				(ft_strlen(s))
+# define S_SIZE(s)				(ft_strlen(s) + 1)
+# define NL						0x0A
+
+/*
+** this enumeration would hold some useful values for the rest
+** of the program like the the tmp buffer indexes, and also
+** return states: success, failure and eof
+*/
+
 enum			e_fuzzbuzz
 {
-	buffer, foo, bar, count,
+	ibuffer, ifoo, ibar, icount,
 
 	failure = -1, eof, success
 };
+
+/*
+** who said C does not have a bool type?
+*/
+
+typedef enum	e_bool
+{
+	false = (1 == 0),
+	true = !false
+}				t_bool;
+
+/*
+** FIXME: do I have to use this? or an array would be perfect!
+*/
 
 typedef struct	s_fileinfo
 {
